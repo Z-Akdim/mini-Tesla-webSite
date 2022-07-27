824793e3ac40019826d12f58ef3dd576a7b48047
Zakarya Akdim <zakaria.akdim37@gmail.com>
2022-07-27 18:42:27 +0100

    newpush

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './index.css'
 function App() {
   return (
     <div className="App">
-      {/* <Header /> */}
+      <Header />
       <Home />
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,68 @@
 import React from 'react'
+import styled from 'styled-components'
+import MenuIcon from '@mui/icons-material/Menu';
 
 function Header() {
   return (
-    <div>Header</div>
+    <Container>
+      <a>
+        <img src="/images/logo.svg" alt=""/>
+      </a>
+      <Menu>
+        <a href="#">Model S</a>
+        <a href="#">Model 3</a>
+        <a href="#">Model X</a>
+        <a href="#">Model Y</a>
+      </Menu>
+      <RightMenu>
+        <a href="#">Shop</a>
+        <a href="#">Tesla Account</a>
+        <CustomMenu />
+      </RightMenu>
+    </Container>
   )
 }
 
 export default Header
+
+const Container = styled.div`
+  min-height: 60px;
+  position: fixed;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 20px;
+  top: 0;
+  left: 0;
+  right: 0;
+`
+
+const Menu = styled.div`
+  display: flex;
+  align-items: center;
+  flex: 1;
+  justify-content: center;
+  a {
+    font-weight: 600;
+    text-transform: uppercase;
+    padding: 0 10px;
+    flex-wrap: nowrap;
+  }
+  @media(max-width: 768px){
+    display: none;
+  }
+`
+
+const RightMenu = styled.div`
+  display: flex;
+  align-items: center;
+  a {
+    font-weight: 600;
+    text-transform: uppercase;
+    margin-right: 10px;
+    flex-wrap: nowrap;
+  }
+`
+const CustomMenu = styled(MenuIcon)`
+  cursor: pointer;
+`
